13b6960332738510f5318933cea8f0aeed3012d2
Troy Porter <porter@fb.com>
2020-10-05 22:04:23 +0100

    1.1 Transitions: Create the Layout

--- src/Transitions/boilerplate/Transitions.tsx
+++ src/Transitions/boilerplate/Transitions.tsx
@@ -1,7 +1,22 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import {
+  Dimensions,
+  ImageStyle,
+  StyleSheet,
+  View,
+  ViewStyle,
+} from "react-native";
 
 import { FlexibleCard as Card, StyleGuide, cards } from "../../components";
+
+interface Layout {
+  id: string;
+  name: string;
+  layout: {
+    container: ViewStyle;
+    child?: ImageStyle;
+  };
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -10,11 +25,50 @@ const styles = StyleSheet.create({
   },
 });
 
+const { width } = Dimensions.get("window");
+
+const column: Layout = {
+  id: "column",
+  name: "Column",
+  layout: {
+    container: {},
+  },
+};
+
+const row: Layout = {
+  id: "row",
+  name: "Row",
+  layout: {
+    container: {
+      flexDirection: "row",
+    },
+  },
+};
+
+const wrap: Layout = {
+  id: "wrap",
+  name: "Wrap",
+  layout: {
+    container: {
+      flexDirection: "row",
+      alignItems: "center",
+      flexWrap: "wrap",
+    },
+    child: {
+      // flex 0 so the width is taken into account?
+      flex: 0,
+      width: width / 2 - StyleGuide.spacing * 2,
+    },
+  },
+};
+
+const currentLayout = wrap.layout;
+
 const Transitions = () => {
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, currentLayout.container]}>
       {cards.map((card) => (
-        <Card key={card.id} style={{}} {...{ card }} />
+        <Card key={card.id} style={currentLayout.child} {...{ card }} />
       ))}
     </View>
   );
