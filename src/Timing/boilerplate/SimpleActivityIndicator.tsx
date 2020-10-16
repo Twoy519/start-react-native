f73acc1b8e24bbd5b0465261b4729f0ec4ee72c2
Troy Porter <porter@fb.com>
2020-10-16 22:51:35 +0100

    fixing types

--- src/Timing/boilerplate/SimpleActivityIndicator.tsx
+++ src/Timing/boilerplate/SimpleActivityIndicator.tsx
@@ -34,7 +34,7 @@ const styles = StyleSheet.create({
 });
 
 interface SimpleActivityIndicatorProps {
-  progress: number;
+  progress: Animated.Value<number>;
 }
 
 const SimpleActivityIndicator = ({
